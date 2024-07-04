95a6fa09b2e2ac49d8fcc38c406c0d40a421cbc1
wahab <www.rex19@gmail.com>
2024-07-04 16:44:09 +0500

    NavBar Animation

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <header>
-      {" "}
       <nav className="navbar fixed top-0 left-0 right-0 w-full max-w-screen-xl mx-auto text-white bg-black mt-4 shadow-md rounded-xl bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4 z-50">
         <div className="navbar-start">
           <div className="dropdown">
@@ -34,19 +33,19 @@ const NavBar = () => {
               tabIndex={0}
               className="menu menu-lg dropdown-content bg-base-100 rounded-box z-[10000] mt-3 w-52 p-2 shadow fixed"
             >
-              <li>
+              <li className="tracking-in-expand">
                 <Link href="/">Home</Link>
               </li>
-              <li>
+              <li className="tracking-in-expand">
                 <Link href="/about">About Us</Link>
               </li>
-              <li>
+              <li className="tracking-in-expand">
                 <Link href="/rent">Rent</Link>
               </li>
-              <li>
+              <li className="tracking-in-expand">
                 <Link href="/buy">Buy</Link>
               </li>
-              <li>
+              <li className="tracking-in-expand">
                 <button
                   className="lg:hidden shiny-btn select-none rounded-lg grad-bg px-4 py-2.5 text-center align-middle font-sans text-md font-extrabold uppercase text-slate-900 shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                   type="button"
@@ -60,7 +59,7 @@ const NavBar = () => {
         <div className="navbar-center">
           <Link href={"/"}>
             <Image
-              className="logo-shadow"
+              className="logo-shadow tracking-in-expand"
               src={Logo}
               alt="al-burraq-logo"
               width={120}
@@ -70,7 +69,7 @@ const NavBar = () => {
         <div className="navbar-end">
           <div className="flex items-center gap-x-1">
             <button
-              className="hidden shiny-btn select-none rounded-lg grad-bg px-4 py-2.5 text-center align-middle font-sans text-md font-extrabold uppercase text-slate-900 shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
+              className="hidden shiny-btn select-none rounded-lg grad-bg px-4 py-2.5 text-center align-middle font-sans text-md font-extrabold uppercase text-slate-900 shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block tracking-in-expand"
               type="button"
             >
               <Link href={"#"}>Instant Call</Link>
