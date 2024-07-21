4a10abd039caa22830b38c607d6df087aad3ad8e
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-07-22 00:05:58 +0500

    Home: Hero section Animation

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BlurFade from "@/components/magicui/blur-fade";
 
 const Hero = () => {
   return (
@@ -11,16 +12,20 @@ const Hero = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
-          <h1 className="mb-5">
-            Your <span className="text-gradient">Dream Property</span>,
-            <br /> Our{" "}
-            <span className="text-gradient">Marketing Expertise</span>.
-          </h1>
-          <p className="mb-5 text-white">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <BlurFade delay={0.25} inView>
+            <h1 className="mb-5 text-5xl">
+              Your <span className="text-gradient">Dream Property</span>,
+              <br /> Our{" "}
+              <span className="text-gradient">Marketing Expertise</span>.
+            </h1>
+          </BlurFade>
+          <BlurFade delay={0.25 * 2} inView>
+            <p className="mb-5 text-xl text-slate-50">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </BlurFade>
         </div>
       </div>
     </section>
