92bab85a2c88c2ff72c5d5654a03750f7039f154
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-07-21 23:53:24 +0500

    Magic Ui Installation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
-import React, { ReactNode } from "react";
 import { AuthProvider } from "@/contexts/AuthContext";
-// import "@/styles/admin.css";
-
-// export const metadata = {
-//   title: "Login",
-// };
+import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
-      {/* <head>
-        <title>{metadata.title}</title>
-      </head> */}
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
