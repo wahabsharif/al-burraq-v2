d7c5441058038a1d957bb2a7a2ed090cd2bb4f84
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-07-25 13:30:07 +0500

    Top Progress Bar

--- src/components/common/ItemsLoader.tsx
+++ src/components/common/ItemsLoader.tsx
@@ -3,13 +3,13 @@ import { DotLoader } from "react-spinners";
 const ItemsLoader = () => (
   <div className="flex justify-center items-center h-screen">
     <div className="m-40">
-      <DotLoader size={60} color={"#123abc"} loading={true} />
+      <DotLoader size={60} color={"rgb(198, 148, 57)"} loading={true} />
     </div>
     <div className="m-40">
-      <DotLoader size={60} color={"#123abc"} loading={true} />
+      <DotLoader size={60} color={"rgb(198, 148, 57)"} loading={true} />
     </div>
     <div className="m-40">
-      <DotLoader size={60} color={"#123abc"} loading={true} />
+      <DotLoader size={60} color={"rgb(198, 148, 57)"} loading={true} />
     </div>
   </div>
 );
--- src/components/layouts/WebLayout.tsx
+++ src/components/layouts/WebLayout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import NavBar from "@/components/common/NavBar";
 import Footer from "@/components/common/Footer";
+import NextTopLoader from "nextjs-toploader";
 import "@/styles/globals.css";
 
 const WebLayout = ({ children }: { children: ReactNode }) => {
@@ -8,6 +9,21 @@ const WebLayout = ({ children }: { children: ReactNode }) => {
     <html lang="en" suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <main>
+          <NextTopLoader
+            color="rgb(198, 148,	57)"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={5}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px rgb(255,230,158),0 0 5px rgb(255,230,158)"
+            template='<div class="bar" role="bar"><div class="peg"></div></div> 
+  <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+            zIndex={1600}
+            showAtBottom={false}
+          />
           <NavBar />
           {children}
           <Footer />
