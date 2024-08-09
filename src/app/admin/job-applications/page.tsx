6b92f4edb50967a8f20f4a536a27d94a944eff5f
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-08-09 10:57:22 +0500

    Renamed

--- src/app/admin/job-applications/page.tsx
+++ src/app/admin/job-applications/page.tsx
@@ -15,7 +15,7 @@ export default function page() {
             className="button flex items-center bg-slate-700 hover:bg-blue-700 text-slate-100 text-xl font-bold py-2 px-4"
             href="/admin/jobs-list"
           >
-            See The Posted Jobs
+            See Posted Jobs
           </Link>
         </div>
 
